79b2012b92b85438ee8a81ba0c1c2f72782b75a2
meiraka <meiraka.pre@gmail.com>
2014-03-15 23:43:59 +0900

    wget apply.sh

--- dep/ubuntu.sh
+++ dep/ubuntu.sh
@@ -3,17 +3,27 @@
 # 
 
 install() {
-    if dpkg -l "$1" > /dev/null ; then
+    if dpkg -l "$1" > /dev/null
+    then
         echo > /dev/null
     else
         sudo apt-get install "$1"
     fi
 }
 
-if grep Ubuntu /etc/issue > /dev/null ; then
+if grep Ubuntu /etc/issue > /dev/null
+    then
     install git
     install zsh
     install tmux
     install vim
     install rake
+    if [ -e ~/local ]
+    then
+        echo ~/local "is already copied." > /dev/null
+    else
+        wget "https://raw.github.com/meiraka/local/master/dep/apply.sh"
+        chmod +x apply.sh
+        sh apply.sh
+    fi
 fi
