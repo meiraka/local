ed84ee05523536bfc203281286b7567f87eae38f
meiraka <meiraka.pre@gmail.com>
2015-01-30 00:34:22 +0900

    split vim plugin and plugins config

--- dotfiles/vim/vimrc.py
+++ dotfiles/vim/vimrc.py
@@ -1,33 +1,10 @@
 """meiraka vim config."""
 
 import os
-import viimproved as vim
 import subprocess
+import viimproved as vim
 
 BUNDLE_DIR = os.path.expanduser('~/.vim-plugins')
-
-if vim.functions.has('gui_running') == '1':
-    vim.settings.background = 'light'
-    vim.settings.guifont = "Migu 1M bold 12"
-    vim.settings.guifontwide = "Migu 1M bold 12"
-else:
-    vim.settings.background = 'dark'
-
-vim.settings.t_Co = 256
-vim.settings.number = True
-vim.settings.cursorline = True
-
-vim.commands.filetype('plugin', 'indent', 'on')
-vim.commands.syntax('on')
-vim.settings.hlsearch = True
-vim.settings.incsearch = True
-vim.settings.backspace = 'start,eol,indent'
-vim.settings.tabstop = 4
-vim.settings.shiftwidth = 4
-vim.settings.expandtab = True
-user = 'y'
-vim.settings.path += ',/home/{0}/include'.format(user)
-# vim.settings.ambiwidth = 'single'
 
 plugin_manager_path = os.path.join(BUNDLE_DIR, 'neobundle.vim')
 if vim.functions.has('vim_starting'):
@@ -55,6 +32,7 @@ if os.path.exists(plugin_manager_path):
     NeoBundleFetch, NeoBundle, NeoBundleCheck = [
         getattr(vim.commands, i) for i in
         ['NeoBundleFetch', 'NeoBundle', 'NeoBundleCheck']]
+    plugins_config = []
 
     neobundle.begin(os.path.expanduser(BUNDLE_DIR))
     NeoBundleFetch("'Shougo/neobundle.vim'")
@@ -64,55 +42,14 @@ if os.path.exists(plugin_manager_path):
 
     if vim.functions.has("lua") == '1':
         NeoBundle("'Shougo/neocomplete.vim'")
-        vim.autoloads.neocomplete.variables.enable_at_startup = 1
 
     if int(vim.variables.version) >= 704:
         NeoBundle("'Shougo/vimshell.vim'")
         NeoBundle("'ujihisa/vimshell-ssh'")
-    NeoBundle("'scrooloose/nerdtree'")
-
-    # syntax checker.
-    # :SyntasticInfo to show checkers
-    NeoBundle("'scrooloose/syntastic'")
-    g.syntastic_python_checkers = ['python', 'pylint',
-                                   'flake8', 'pep257']
-    g.syntastic_cpp_cpplint_args = ('--verbose=3 '
-                                    'filter=-legal/copyright '
-                                    '--extensions=hpp,cpp ')
-    g.syntastic_cpp_check_header = 1
-    g.syntastic_cpp_no_include_search = 1
-    g.syntastic_cpp_auto_refresh_includes = 1
-    g.syntastic_cpp_include_dirs = [i for i in vim.settings.path.split(',') if i]
-
-    # Status line
-    NeoBundle("'itchyny/lightline.vim'")
-    vim.settings.laststatus = 2
-    g.lightline = {'colorscheme': 'wombat',
-                   'active': {'right': [['syntastic', 'lineinfo'],
-                                        ['percent'],
-                                        ['fileformat', 'fileencoding',
-                                         'filetype']]},
-                   'component_expand': {
-                       'syntastic': 'SyntasticStatuslineFlag'},
-                   'component_type': {'syntastic': 'error'}}
-
-    g.syntastic_mode_map = {'mode': 'passive'}
-
-    def show_syntastic_errors():
-        """check syntastic and show errors to lightline."""
-        vim.commands.SyntasticCheck()
-        vim.autoloads.lightline.functions.update()
-    vim.autocmd.bind('BufWritePost', '*',
-                     show_syntastic_errors, [])
-
-    # NeoBundle("'kien/ctrlp.vim'")
-    NeoBundle("'nathanaelkane/vim-indent-guides'")
-    g.indent_guides_enable_on_vim_startup = 1
-    g.indent_guides_start_level = 2
-    g.indent_guides_color_change_percent = 30
-    g.indent_guides_guide_size = 1
-
-    NeoBundle("'tpope/vim-fugitive'")
+    NeoBundle("'scrooloose/syntastic'")  # syntax checker
+    NeoBundle("'itchyny/lightline.vim'")  # statusline
+    NeoBundle("'nathanaelkane/vim-indent-guides'")  # indent view
+    NeoBundle("'tpope/vim-fugitive'")  # git
 
     # Lang: C++
     NeoBundle("'meiraka/vim-google-cpp-style-indent'")
@@ -123,9 +60,6 @@ if os.path.exists(plugin_manager_path):
 
     # Lang: Haskell
     NeoBundle("'kana/vim-filetype-haskell'")
-    # NeoBundle("'raichoo/haskell-vim'")
-    # vim.globals.variables.haskell_enable_quantification = 1
-    # vim.globals.variables.haskell_enable_arrowsyntax = 1
 
     # Lang: Vim
     NeoBundle("'vim-jp/vimdoc-ja'")
@@ -137,16 +71,85 @@ if os.path.exists(plugin_manager_path):
     vim.commands.filetype("plugin", "indent", "on")
     NeoBundleCheck()
 
-if neobundle.is_installed('jedi-vim') == '1':
     if neobundle.is_installed('neocomplete.vim') == '1':
+        vim.autoloads.neocomplete.variables.enable_at_startup = 1
+    if neobundle.is_installed('vim-indent-guides') == '1':
+        g.indent_guides_enable_on_vim_startup = 1
+        g.indent_guides_start_level = 2
+        g.indent_guides_color_change_percent = 30
+        g.indent_guides_guide_size = 1
+    if neobundle.is_installed('syntastic') == '1':
+        g.syntastic_python_checkers = ['python', 'pylint',
+                                       'flake8', 'pep257']
+        g.syntastic_cpp_cpplint_args = ('--verbose=3 '
+                                        'filter=-legal/copyright '
+                                        '--extensions=hpp,cpp ')
+        g.syntastic_cpp_check_header = 1
+        g.syntastic_cpp_no_include_search = 1
+        g.syntastic_cpp_auto_refresh_includes = 1
+
+        def apply_path_to_syntastic():
+            """Apply current vim path to syntastic."""
+            g.syntastic_cpp_include_dirs = [
+                i for i in vim.settings.path.split(',') if i]
+
+        vim.autocmd.bind('FileType', 'cpp', apply_path_to_syntastic, [])
+
+    if neobundle.is_installed('lightline.vim') == '1':
+        vim.settings.laststatus = 2
+        g.lightline = {'colorscheme': 'wombat',
+                       'active': {'right': [['syntastic', 'lineinfo'],
+                                            ['percent'],
+                                            ['fileformat', 'fileencoding',
+                                             'filetype']]},
+                       'component_expand': {
+                           'syntastic': 'SyntasticStatuslineFlag'},
+                       'component_type': {'syntastic': 'error'}}
+        if neobundle.is_installed('syntastic') == '1':
+            g.syntastic_mode_map = {'mode': 'passive'}
+
+            def show_syntastic_errors():
+                """check syntastic and show errors to lightline."""
+                vim.commands.SyntasticCheck()
+                vim.autoloads.lightline.functions.update()
+
+            vim.autocmd.bind('BufWritePost', '*',
+                             show_syntastic_errors, [])
+
+    if (neobundle.is_installed('jedi-vim') == '1' and
+            neobundle.is_installed('neocomplete.vim') == '1'):
         vim.autocmd.bind('FileType', 'python',
                          vim.commands.setlocal, ['omnifunc=jedi#completions'])
         vim.autoloads.jedi.variables.completions_enabled = 0
         vim.autoloads.jedi.variables.auto_vim_configuration = 0
         vim.autoloads.neocomplete.variables.force_omni_input_patterns = {
-            'python': '\%([^. \t]\.\|^\s*@\|^\s*from\s.\+import \|^\s*from \|^\s*import \)\w*'}
+            'python': '\%([^. \t]\.\|^\s*@\|'
+                      '^\s*from\s.\+import \|^\s*from \|^\s*import \)\w*'}
 
-vim.commands.colorscheme("tricolore")
+    vim.commands.colorscheme("tricolore")
+
+if vim.functions.has('gui_running') == '1':
+    vim.settings.background = 'light'
+    vim.settings.guifont = "Migu 1M bold 12"
+    vim.settings.guifontwide = "Migu 1M bold 12"
+else:
+    vim.settings.background = 'dark'
+
+vim.settings.t_Co = 256
+vim.settings.number = True
+vim.settings.cursorline = True
+
+vim.commands.filetype('plugin', 'indent', 'on')
+vim.commands.syntax('on')
+vim.settings.hlsearch = True
+vim.settings.incsearch = True
+vim.settings.backspace = 'start,eol,indent'
+vim.settings.tabstop = 4
+vim.settings.shiftwidth = 4
+vim.settings.expandtab = True
+user = 'y'
+vim.settings.path += ',/home/{0}/include'.format(user)
+# vim.settings.ambiwidth = 'single'
 
 
 def reload():
